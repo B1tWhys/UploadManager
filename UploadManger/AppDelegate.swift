2c2ba5398d76047221cb9c401f3d428ecf495c11
boldersky <skyarnold1@me.com>
2015-04-26 16:09:45 -0600

    I setup the xcode project, I also added some notes.

--- UploadManger/AppDelegate.swift
+++ UploadManger/AppDelegate.swift
@@ -9,15 +9,21 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate  {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        var request = NSMutableURLRequest(URL: NSURL(string: "https://accounts.google.com/o/oauth2/auth?client_id=852892140756-oqbpkk811aojkh2ef25lq5fn1micvnv7.apps.googleusercontent.com&redirect_uri=http://localhost&response_type=code&scope=https://www.googleapis.com/auth/youtube.upload")!)
+        var session = NSURLSession.sharedSession()
+        request.HTTPMethod = "POST"
+        
+        var params = ["client_id": "852892140756-oqbpkk811aojkh2ef25lq5fn1micvnv7.apps.googleusercontent.com", "redirect_uri": "http://localhost:port", "response_type": "code", "scope": "*"]
+        var err: NSError?
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
